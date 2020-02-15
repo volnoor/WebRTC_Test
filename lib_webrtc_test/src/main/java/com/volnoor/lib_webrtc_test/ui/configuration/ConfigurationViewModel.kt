721d00854902e81053019db4fdffaac26aa8ee77
Yevhenii <volnoor@gmail.com>
2020-02-15 16:53:22 +0200

    Add room text observing

--- lib_webrtc_test/src/main/java/com/volnoor/lib_webrtc_test/ui/configuration/ConfigurationViewModel.kt
+++ lib_webrtc_test/src/main/java/com/volnoor/lib_webrtc_test/ui/configuration/ConfigurationViewModel.kt
@@ -1,13 +1,25 @@
 package com.volnoor.lib_webrtc_test.ui.configuration
 
 import android.util.Log
+import androidx.databinding.ObservableField
+import androidx.databinding.adapters.TextViewBindingAdapter
 import com.volnoor.lib_webrtc_test.ui.base.BaseViewModel
 
 class ConfigurationViewModel : BaseViewModel<ConfigurationNavigator>() {
 
+    val isLoading = ObservableField<Boolean>()
+
+    private var textRoom: String? = null
+
+    val roomTextChangeListener = TextViewBindingAdapter.AfterTextChanged() { text ->
+        Log.d(TAG, "afterTextChanged: $text")
+        textRoom = text.toString()
+    }
+
     fun onJoinClicked() {
-        Log.d(TAG, "onJoinClicked")
-        getNavigator().showCallScreen()
+        Log.d(TAG, "onJoinClicked: $textRoom")
+
+        isLoading.set(true)
     }
 
     companion object {
